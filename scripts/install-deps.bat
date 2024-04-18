fe1b09688eb727086ae9711e1c744da85f9e1d58
nukbal <frostwolfclaw@gmail.com>
2024-04-18 23:34:32 +0900

    fix: add script for install external dependencies

---
+++ scripts/install-deps.bat
@@ -0,0 +1,27 @@
+@echo off
+
+choco install pkgconfiglite
+vcpkg install dav1d:x64-windows
+
+$VCINSTALLDIR = $(& "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath)
+Add-Content $env:GITHUB_ENV "LIBCLANG_PATH=${VCINSTALLDIR}\VC\Tools\LLVM\x64\bin`n"
+Invoke-WebRequest "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.7z" -OutFile ffmpeg-release-full-shared.7z
+7z x ffmpeg-release-full-shared.7z
+mkdir ffmpeg
+mv ffmpeg-*/* ffmpeg/
+mkdir src-tauri/bin
+mv ffmpeg/bin/ffmpeg.exe src-tauri/bin/ffmpeg-x86_64-pc-windows-msvc.exe
+
+
+$VCINSTALLDIR = $(& "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath)
+Add-Content $env:GITHUB_ENV "LIBCLANG_PATH=${VCINSTALLDIR}\VC\Tools\LLVM\x64\bin`n"
+Invoke-WebRequest "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-windows.zip" -OutFile realesrgan.zip
+7z x realesrgan.zip
+mv realesrgan-ncnn-vulkan.exe src-tauri/bin/realesrgan-x86_64-pc-windows-msvc.exe
+mv vcomp140.dll src-tauri/bin/vcomp140.dll
+mv vcomp140d.dll src-tauri/bin/vcomp140d.dll
+mkdir src-tauri/models
+mv models/realesr-animevideov3-x2.bin src-tauri/models/realesr-animevideov3-x2.bin
+mv models/realesr-animevideov3-x2.param src-tauri/models/realesr-animevideov3-x2.param
+mv models/realesr-animevideov3-x4.bin src-tauri/models/realesr-animevideov3-x4.bin
+mv models/realesr-animevideov3-x4.param src-tauri/models/realesr-animevideov3-x4.param
---
+++ scripts/install-deps.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# install dav1d
+brew install dav1d
+
+# install ffmpeg
+curl -JL -o ./ffmpeg.7z https://evermeet.cx/ffmpeg/get
+7z x ffmpeg.7z
+mkdir src-tauri/bin
+mv ffmpeg src-tauri/bin/ffmpeg-x86_64-apple-darwin
+
+# Add Real-ESRGAN anime models
+
+curl -OL https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-macos.zip
+7z x realesrgan-ncnn-vulkan-20220424-macos.zip
+mv realesrgan-ncnn-vulkan src-tauri/bin/realesrgan-x86_64-apple-darwin
+mkdir src-tauri/models
+mv models/realesr-animevideov3-x2.bin src-tauri/models/realesr-animevideov3-x2.bin
+mv models/realesr-animevideov3-x2.param src-tauri/models/realesr-animevideov3-x2.param
+mv models/realesr-animevideov3-x4.bin src-tauri/models/realesr-animevideov3-x4.bin
+mv models/realesr-animevideov3-x4.param src-tauri/models/realesr-animevideov3-x4.param
